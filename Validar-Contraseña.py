8a1379f3127c6444549d2b2fea82939dc8520649
Sebastián Diaz <99108952+SebasMas@users.noreply.github.com>
2022-12-22 13:46:49 -0500

    EasyProgram

---
+++ Validar-Contraseña.py
@@ -0,0 +1,15 @@
+
+OriginalPassword = "GitHub2020"
+password = input("Ingrese la contraseña: ")
+
+if password == OriginalPassword.lower():
+    print("Contraseña correcta")
+
+else:
+    print("Coma monda malparido")
+    
+    print("Careverga")
+    print("Mojon de piso")
+    print("Firifiri")
+    print("foroforo")
+    
